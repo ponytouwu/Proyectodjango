678bf98901927d437c255fbdc6847a26227c3455
ponytouwu <ni.cabrerac@duocuc.cl>
2021-05-28 18:20:41 -0400

    AVANCE DE TABLAS
    TABLAS REGION, COMUNA, DIRECCION,TIPO_USUARIO,USUARIO

--- proyectoDjango/nucleo/admin.py
+++ proyectoDjango/nucleo/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from .models import Categoria,Marca,Producto,Contacto
+from .models import Categoria,Marca,Producto,Contacto,Usuario,Tipo_usuario,Region,Comuna,Direccion
 # Register your models here.
 
 admin.site.register(Categoria)
 admin.site.register(Marca)
 admin.site.register(Producto)
 admin.site.register(Contacto)
+admin.site.register(Direccion)
+admin.site.register(Comuna)
+admin.site.register(Tipo_usuario)
+admin.site.register(Usuario)
+admin.site.register(Region)
--- proyectoDjango/nucleo/models.py
+++ proyectoDjango/nucleo/models.py
@@ -43,3 +43,60 @@ class Contacto(models.Model):
  
     def __str__(self): 
         return self.p_nombre 
+
+#NICO
+class Region(models.model):
+    id_region = models.IntegerField(primary_key=True, verbose_name='Id de la region')
+    nombre_region = models.CharField(max_length=100, blank=False, verbose_name='Nombre region')
+    
+    def __str__(self):
+        return self.nombre_region
+
+class Comuna(models.model):
+    id_comuna = models.IntegerField(primary_key=True, verbose_name='Id de la comuna')
+    nombre_comuna = models.CharField(max_length=100, blank=False, verbose_name='Nombre comuna')
+    region = models.ForeignKey(Marca, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombre_comuna
+
+
+class Direccion(models.model):
+    id_direccion = models.IntegerField(primary_key=True, verbose_name='Id de la direccion')
+    descrip_dir = models.CharField(max_length=100, blank=False, verbose_name='Descripcion de la direccion')
+    comuna = models.ForeignKey(Marca, on_delete=models.CASCADE)
+    #falta crear tabla usuario para unirla aqui como las foreanas, YA CREADA PARECE
+    Usuario = models.ForeignKey(Marca, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.descrip_dir
+
+
+
+class Tipo_usuario(models.model):
+    id_usu_tip = models.IntegerField(primary_key=True, verbose_name='Id del usuario')
+    nom_usu = models.CharField(max_length=50, blank=False, verbose_name='Tipo de usuario')
+    
+    def __str__(self):
+        return self.nom_usu
+
+
+#tabla usario hay que terminar de rellenar, ESTARIA LISTA
+
+
+class Usuario(models.Model):
+    id_usuario = models.IntegerField(primary_key=True, verbose_name='id del contacto')
+    nombre_u = models.CharField(max_length=100, blank=False, verbose_name='nombre usuario')
+    apellido_u = models.CharField(max_length=100, blank=False, verbose_name='apellido usuario')
+    email_u = models.CharField(max_length=100, blank=False, verbose_name='correo del usuario')
+    telofono_u = models.CharField(max_length=11, blank=False, verbose_name='telefono del usuario')
+    contraseña_u= models.CharField(max_length=20, blank=False, verbose_name='comentario del usuario')
+    run_u = models.IntegerField(blank=False, verbose_name='run del usuario')
+    dv_run = models.CharField(max_length=1, blank=False, verbose_name='digito verificador del usuario')
+    cod_post = models.IntegerField(blank=False, verbose_name='codigo postal del usuario')
+    modo_osc = models.IntegerField(blank=False, verbose_name='modo oscuro/modo normal')
+    tipo_usuario = models.ForeignKey(Marca, on_delete=models.CASCADE)
+    
+    def __str__(self): 
+        return self.run_u 
+
+#AQUI TERMINA LO MIO (NICO), FALTARIA ORDENAR SOLAMENTE
