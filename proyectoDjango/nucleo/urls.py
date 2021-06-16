20a5a676b7d27d2b6303fc7710eb9d376e160ee4
ElPirocas <gnar2342@gmail.com>
2021-06-16 18:05:53 -0400

    xsd
    xd

--- proyectoDjango/nucleo/urls.py
+++ proyectoDjango/nucleo/urls.py
@@ -2,7 +2,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth.views import LoginView, LogoutView
-from .views import home, seccionotros , contactanos, secciongatuna, Seccionperruna, registro, carrito, cambiocontra, recuperacion,lista_regiones,guardar_usuario,guardar_comentario, listado,eliminar_listado,listar_tablas,modificar_pro,agregar_carr,eliminar_carro,guardar_producto,agregar_p,mostrar_p,carro1,login_view,logout_view
+from .views import home, inicioSesion, seccionotros , contactanos, secciongatuna, Seccionperruna, registro, carrito, cambiocontra, recuperacion,lista_regiones,guardar_usuario,guardar_comentario, listado,eliminar_listado,listar_tablas,modificar_pro,agregar_carr,eliminar_carro,guardar_producto,agregar_p,mostrar_p,carro1,login_view,logout_view
 urlpatterns = [
     path('', home, name="home"),
     path('mostrar_p/<int:id>', mostrar_p, name="mostrar_p"),
@@ -13,6 +13,7 @@ urlpatterns = [
     path('inicioSesion/', LoginView.as_view(template_name = 'nucleo/inicioSesion.html'), name="inicioSesion"),
     #path('CerrarSesion/', LogoutView.as_view(template_name = 'nucleo/home.html'),name='CerrarSesion'),
     path('sesion/',login_view,name='sesion'),
+    path('inicioSesion',inicioSesion,name='inicioSesion'),
     path('CerrarSesion/',logout_view, name='CerrarSesion'),
     path('registro', registro, name="registro"),
     path('carrito',carrito,name="carrito"),
